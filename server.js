4651aa9634989485cfea252dbed1839d6a280299
Nilrudra1999 <nilrudram@gmail.com>
2025-01-16 17:58:31 -0500

    continuing work on assignment 1

--- server.js
+++ server.js
@@ -9,25 +9,23 @@ const app = express();
 app.use(cors());    // declared before the routes
 app.use(express.json());
 const db = new MoviesDB();
+// Using 3000 as a fallback when the .env file is not working or not being read properly
+const HTTP_PORT = process.env.PORT || 3000;
 
 
 // Initializing the database connection using the environment variables file 
 db.initialize(process.env.MONGODB_CONN_STRING)
     .then(() => { 
-        console.log("Database connection successful!"); 
+        app.listen(HTTP_PORT, () => {
+            console.log(`Server listening on: ${HTTP_PORT}`);
+        });
     })
     .catch((err) => {
-        console.error("Database connection failed:", err); 
-        process.exit(1); 
+        console.error(err); 
+        process.exit(1);    // db initialization failed
     });
 
-
+// Basic GET route for testing
 app.get('/', (req, res) => {
     res.json({message: "API Listening"});
-});
-
-// Using this as a fallback when the .env file is not working or not being read properly
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
 });
